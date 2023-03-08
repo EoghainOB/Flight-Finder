0491749bfb2c7477b85cd4805a6dfbc4d0299fc3
Eoghain O'Briain <eoghain.obriain@appliedtechnology.se>
2023-03-08 17:05:19 +0100

    Expanding the flight query

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ app.listen(8080, () => {
   console.log("Server running on port %PORT%".replace("%PORT%", 8080));
 });
 
-app.get("/", (req, res, next) => {
-  res.json({ message: "Ok" });
-});
-
 //Flights endpoints
 
 app.get("/api/flights", (req, res, next) => {
@@ -41,20 +37,53 @@ app.get("/api/flightsearch", (req, res) => {
   const priceRangeHigh = req.body.priceRangeHigh;
   const priceRangeLow = req.body.priceRangeLow;
 
-  const query = `SELECT *
-                FROM FlightItineraries AS fi
-                JOIN FlightRoutes AS fr ON fi.route_id = fr.route_id
-                JOIN FlightPrices AS fp ON fi.itinerary_flight_id = fp.itinerary_flight_id
-                WHERE fr.departureDestination = ? 
-                AND fr.arrivalDestination = ?
-                AND DATE(fi.departureAt) = DATE(?)
-                AND fi.availableSeats >= ?
-                AND fp.adult <= ?
-                AND fp.adult >= ?`;
+  const query = `
+      SELECT fi.itinerary_flight_id, fi.departureAt, fi.arrivalAt, fi.availableSeats, fp.adult, fr1.departureDestination, fr1.arrivalDestination
+      FROM FlightItineraries AS fi
+      JOIN (
+        SELECT fr1.route_id AS departureRouteId, fr2.route_id AS arrivalRouteId
+        FROM FlightRoutes AS fr1
+        JOIN FlightRoutes AS fr2 ON fr1.arrivalDestination = fr2.departureDestination
+        WHERE fr1.departureDestination = ?
+          AND fr2.arrivalDestination = ?
+      ) AS routes ON fi.route_id = routes.departureRouteId OR fi.route_id = routes.arrivalRouteId
+      JOIN FlightRoutes AS fr1 ON fi.route_id = fr1.route_id
+      JOIN FlightPrices AS fp ON fi.itinerary_flight_id = fp.itinerary_flight_id
+      WHERE (
+        (fr1.departureDestination = ? OR fr1.arrivalDestination = ?)
+        AND DATE(fi.departureAt) = DATE(?)
+        AND fi.availableSeats >= ?
+        AND fp.adult <= ?
+        AND fp.adult >= ?
+      )
+      
+      UNION ALL
+      
+      SELECT fi.itinerary_flight_id, fi.departureAt, fi.arrivalAt, fi.availableSeats, fp.adult, fr.departureDestination, fr.arrivalDestination
+      FROM FlightItineraries AS fi
+      JOIN FlightRoutes AS fr ON fi.route_id = fr.route_id
+      JOIN FlightPrices AS fp ON fi.itinerary_flight_id = fp.itinerary_flight_id
+      WHERE fr.departureDestination = ? 
+      AND fr.arrivalDestination = ?
+      AND DATE(fi.departureAt) = DATE(?)
+      AND fi.availableSeats >= ?
+      AND fp.adult <= ?
+      AND fp.adult >= ?
+
+      ORDER BY departureAt
+  `;
 
   db.all(
     query,
     [
+      departureDestination,
+      arrivalDestination,
+      departureDestination,
+      arrivalDestination,
+      departureAt,
+      seats,
+      priceRangeHigh,
+      priceRangeLow,
       departureDestination,
       arrivalDestination,
       departureAt,
@@ -66,43 +95,6 @@ app.get("/api/flightsearch", (req, res) => {
       if (!rows || rows.length === 0) {
         res.status(404).send("No flights found that meet your requirements");
       } else if (err) {
-        res.status(500).send({ error: err.message });
-      } else {
-        res.send(rows);
-      }
-    }
-  );
-});
-
-app.post("/api/flightbook", (req, res) => {
-  const departureDestination = req.body.departureDestination;
-  const arrivalDestination = req.body.arrivalDestination;
-  const departureAt = new Date(req.body.departureAt).toISOString();
-  const seats = req.body.seats;
-
-  const query = `SELECT *
-                FROM FlightItineraries AS fi
-                JOIN FlightRoutes AS fr ON fi.route_id = fr.route_id
-                WHERE fr.departureDestination = ? 
-                AND fr.arrivalDestination = ?
-                AND DATE(fi.departureAt) = DATE(?)
-                AND fi.availableSeats >= ?
-                JOIN FlightPrices AS fp ON fi.itinerary_flight_id = fr.itinerary_flight_id
-                WHERE adult <= priceRangeHigh
-                AND adult >= priceRangeLow`;
-
-  db.all(
-    query,
-    [
-      departureDestination,
-      arrivalDestination,
-      departureAt,
-      seats,
-      priceRangeHigh,
-      priceRangeLow,
-    ],
-    (err, rows) => {
-      if (err) {
         res.status(500).send({ error: err.message });
       } else {
         res.send(rows);
